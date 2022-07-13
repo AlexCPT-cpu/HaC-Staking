7c563c4c7a58ecd4793f0609efd69a9f8ad45b01
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2022-07-13 23:54:51 +0100

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,7 @@
+import { createAlchemyWeb3 } from '@alch/alchemy-web3';
+export const NFTCONTRACT = "0xAFc7647b584730694B9606511F11F423A0816eFf";
+export const STAKINGCONTRACT = "0x32b598a60ebe5f3b06d4b9d6571d2a8d9fe0d7e8";
+export const bscscanapi = "https://api.bscscan.com/api";
+export const moralisapi = "https://deep-index.moralis.io/api/v2/";
+export const nftpng = "https://ipfs.io/ipfs/QmYh538cj3pfW1uPeKB86ihMNuaAitoZv14T2ZFjxoPjBj/";
+export const Web3Alc = createAlchemyWeb3("https://polygon-mumbai.g.alchemy.com/v2/8AX5AP2TU6G45ctsOXNt8K4Fz_BkhhZG");
