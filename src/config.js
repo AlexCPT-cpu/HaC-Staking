e6b49404803dc75f813b72b2bb6dc7e3198f28f8
AlexCPT-cpu <67722561+AlexCPT-cpu@users.noreply.github.com>
2022-07-13 23:54:17 +0100

    Delete src directory

--- src/config.js
+++
@@ -1,7 +0,0 @@
-import { createAlchemyWeb3 } from '@alch/alchemy-web3';
-export const NFTCONTRACT = "0xD2F1E4584E81115aDF28c15b5d3a757bDEA556d4";
-export const STAKINGCONTRACT = "0x97E935a12FF1ad32beB623A1907093aB2279f157";
-export const polygonscanapi = "https://api-testnet.polygonscan.com/api";
-export const moralisapi = "https://deep-index.moralis.io/api/v2/";
-export const nftpng = "https://ipfs.io/ipfs/QmYh538cj3pfW1uPeKB86ihMNuaAitoZv14T2ZFjxoPjBj/";
-export const Web3Alc = createAlchemyWeb3("https://polygon-mumbai.g.alchemy.com/v2/8AX5AP2TU6G45ctsOXNt8K4Fz_BkhhZG");
